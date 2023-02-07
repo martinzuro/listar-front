04d9c5125f169d0faf2ed2b96f2deadc9b2cdcad
Martin zuro <martinzuro@gmail.com>
2023-02-06 23:33:19 -0300

    add ably

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 
+import { configureAbly } from "@ably-labs/react-hooks";
+
+configureAbly({ key: process.env.NEXT_PUBLIC_ABLY_API_KEY });
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
